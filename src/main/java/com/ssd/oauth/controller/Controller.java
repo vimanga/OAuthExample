4b556ca0f7d046ed6762c22793be0da5409293e8
vimanga <balallav@gmail.com>
2020-09-29 20:16:31 +0530

    jsp fixed

--- src/main/java/com/ssd/oauth/controller/Controller.java
+++ src/main/java/com/ssd/oauth/controller/Controller.java
@@ -2,23 +2,65 @@ package com.ssd.oauth.controller;
 
 import java.security.Principal;
 
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
+import net.minidev.json.parser.ParseException;
 
 @RestController
+@SessionAttributes("name")
 public class Controller {
 
+	@SuppressWarnings("deprecation")
 	@RequestMapping("/")
-	public String home()
+	public ModelAndView home(ModelMap model, Principal principal) throws JsonProcessingException, ParseException
 	{
-		return "xxx";
 		
+		JSONParser parser = new JSONParser(); 
+		//get the principal into JSON format
+		JSONObject json = (JSONObject) parser.parse(userjson(principal));
+		
+		//get array inside authorities
+		JSONArray jsonnew = (JSONArray) parser.parse(json.getAsString("authorities"));
+		
+		//get the first element in JSON array to JSON object
+		JSONObject jsonnewnew = (JSONObject) parser.parse(jsonnew.get(0).toString());
+		
+		//access the attributes section in the previous JSON object.
+		JSONObject jsonnewnewname = (JSONObject) parser.parse(jsonnewnew.getAsString("attributes"));
+		
+		System.out.println("****************"+jsonnewnewname.getAsString("name"));
+		
+		String name = jsonnewnewname.getAsString("name");
+		model.put("name", name);
+		ModelAndView mav = new ModelAndView("login");
+		return mav;
 	}
 	
 	@RequestMapping("user")
-	@ResponseBody
-	public Principal user(Principal principal) {
+//	@ResponseBody
+	public Principal user(Principal principal) throws JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+	    String user = mapper.writeValueAsString(principal);
+	    System.out.println(user);
 		return principal;
+	}
+	
+	@RequestMapping("userjson")
+	public String userjson(Principal principal) throws JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+	    String user = mapper.writeValueAsString(principal);
+	    //System.out.println(user);
+		return user;
 	}
 }
